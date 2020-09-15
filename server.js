e9437bfbf94d80a1ee65afdaa052d3e2874d8ff4
Alvaro Centeno <alvaroc0689@gmail.com>
2020-09-15 13:02:58 -0700

    added functionality to add dept, delete emp, update role, and get emp, mgr, and dept query

--- server.js
+++ server.js
@@ -130,3 +130,275 @@ function renderDept() {
         });
 }
 
+function addEmp() {
+    managerQuery()
+        .then(res => {
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "First name:",
+                        name: "first"
+                    },
+                    {
+                        type: "input",
+                        message: "Last name:",
+                        name: "last"
+                    },
+                    {
+                        type: "list",
+                        message: "Role:",
+                        name: "role_id",
+                        choices: [
+
+                            "1 - Sales Lead",
+                            "2 - Salesperson",
+                            "3 - Lead Engineer",
+                            "4 - Software Engineering",
+                            "5 - Finance",
+                            "6 - Legal Team Lead",
+                            "7 - Lawyer"
+
+
+                        ]
+                    }
+                ])
+                .then(data => {
+                    let title = parseInt(data.role_id.charAt(0));
+                    let first_name = data.first;
+                    let last_name = data.last;
+                    let noMgr = null;
+                    if (
+                        title === 2 ||
+                        title === 4 ||
+                        title === 5 ||
+                        title === 6
+                    ) {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "list",
+                                    message: "Manager:",
+                                    name: "manager",
+                                    choices: mgrArr
+                                }
+                            ])
+                            .then(data => {
+                                noMgr = parseInt(data.manager.charAt(0));
+                                const queryString = `INSERT INTO employee_db.employee_table (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`;
+                                connection.query(
+                                    queryString,
+                                    [first_name, last_name, title, noMgr],
+                                    function (err) {
+                                        if (err) throw err;
+                                        console.log("Welcome to the team");
+                                        employeeManager();
+                                    }
+                                );
+                            });
+                    } else {
+                        console.log(title);
+                        console.log(data);
+                        connection.query(
+                            `INSERT INTO employee_db.employee_table (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`,
+                            [first_name, last_name, title, noMgr],
+                            function (err) {
+                                if (err) throw err;
+                                console.log("Welcome to the team");
+                                employeeManager();
+                            }
+                        );
+                    }
+                });
+        })
+        .catch(console.error);
+}
+
+function addRole() {
+    departmentQuery()
+        .then(res => {
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message:
+                            "What role would you like to add?",
+                        name: "roleName"
+                    },
+                    {
+                        type: "input",
+                        message: "What will the salary be for this new role?",
+                        name: "salary"
+                    },
+                    {
+                        type: "list",
+                        message:
+                            "Which department will this new role be in under?",
+                        name: "dept",
+                        choices: deptArr
+                    }
+                ])
+                .then(data => {
+                    let deptNum = parseInt(data.dept.charAt(0));
+                    const queryString =
+                        "INSERT INTO employee_db.role_table (title, salary, department_id) values (?, ?, ?);";
+                    connection.query(
+                        queryString,
+                        [data.roleName, data.salary, deptNum],
+                        function (err) {
+                            if (err) throw err;
+                            console.log("Role Added!");
+                            employeeManager();
+                        }
+                    );
+                });
+        })
+        .catch(console.error);
+};
+
+function addDept() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "New department name:",
+                name: "deptName"
+            }
+        ])
+        .then(data => {
+            const queryString = `INSERT INTO employee_db.department_table (dept_name) values (?);`;
+            connection.query(queryString, [data.deptName], (err) => {
+                if (err) throw err;
+                console.log("Department added!");
+                employeeManager();
+            });
+        });
+};
+
+
+
+const deleteEmp = (id) => {
+    return new Promise(() => {
+        connection
+            .query(`DELETE FROM employee_db.employee_table WHERE id = ?`, [id], (err) => {
+                if (err) throw err;
+            })
+    })
+}
+
+const deleteEmployee = async () => {
+
+    let empList = await empSelect()
+
+    try {
+        inquirer.prompt([
+            {
+                name: "empDelete",
+                message: "Delete which employee?",
+                type: "list",
+                choices: empList.map(e => { return { name: e.first_name + " " + e.last_name, value: e.id } })
+
+            },
+            {
+                name: "confirm",
+                message: "Delete?",
+                type: "confirm",
+            },
+        ]).then((res) => {
+
+            console.log('Employee Deleted');
+
+            if (res.confirm === true) {
+                deleteEmp(res.empDelete)
+            }
+            employeeManager()
+        })
+    } catch (err) {
+        console.log(err);
+
+    }
+}
+
+function updateRole() {
+    employeeQuery()
+        .then(result => {
+            inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        message:
+                            "Choose the employee's role that you would you like to update",
+                        name: "employee",
+                        choices: empArr
+                    },
+                    {
+                        type: "list",
+                        message: "choose the role of the employee:",
+                        name: "newRole",
+                        choices: [
+                            "1 - Sales Lead",
+                            "2 - Salesperson",
+                            "3 - Lead Engineer",
+                            "4 - Software Engineer",
+                            "5 - Accountant",
+                            "6 - Legal Team Lead",
+                            "7 - Lawyer"
+                        ]
+                    }
+                ])
+                .then(data => {
+                    const title = parseInt(data.newRole.charAt(0));
+                    const queryString = `UPDATE employee_db.employee_table SET role_id=? WHERE first_name=?;`;
+                    connection.query(
+                        queryString,
+                        [title, data.employee],
+                        function (err, data) {
+                            if (err) throw err;
+                            console.table("Role Updated!");
+                            employeeManager();
+                        }
+                    );
+                });
+        })
+        .catch(console.error);
+}
+
+function employeeQuery() {
+    return new Promise((resolve, reject) => {
+        const queryString = `SELECT * FROM employee_db.employee_table;`;
+        connection.query(queryString, (err, data) => {
+            if (err) reject(err);
+            array = data.map(emp => emp.first_name);
+            empArr = array;
+            resolve(empArr);
+        });
+    });
+}
+
+function managerQuery() {
+    return new Promise((resolve, reject) => {
+        const queryString = `SELECT * FROM employee_db.employee_table;`;
+        connection.query(queryString, (err, data) => {
+            if (err) reject(err);
+            let array = data
+                .filter(mgr => {
+                    return mgr.manager_id === null;
+                })
+                .map(x => x.id + " " + x.first_name);
+            mgrArr = array;
+            resolve(mgrArr);
+        });
+    });
+}
+
+function departmentQuery() {
+    return new Promise((resolve, reject) => {
+        const queryString = `SELECT * FROM employee_db.department_table;`;
+        connection.query(queryString, (err, data) => {
+            if (err) reject(err);
+            let array = data.map(dept => dept.id + " " + dept.dept_name);
+            deptArr = array;
+            resolve(deptArr);
+        });
+    });
+}
